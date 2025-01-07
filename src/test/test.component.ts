718ab3bace1c351c98c9e4568610ee8ad66cb3c5
SeraphCoding <yannikland@gmail.com>
2025-01-07 18:34:30 +0100

    Add Material Button testcase

--- src/test/test.component.ts
+++ src/test/test.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatChip} from '@angular/material/chips';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-test',
-  imports: [],
+  imports: [
+    MatChip,
+    MatButton
+  ],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
