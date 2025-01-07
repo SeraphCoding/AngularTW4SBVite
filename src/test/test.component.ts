515e7361ee63bc7a3f7a7d883c4b56ae5d050cf8
SeraphCoding <yannikland@gmail.com>
2025-01-07 18:39:26 +0100

    remove unused import

--- src/test/test.component.ts
+++ src/test/test.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import {MatChip} from '@angular/material/chips';
 import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-test',
   imports: [
-    MatChip,
     MatButton
   ],
   templateUrl: './test.component.html',
