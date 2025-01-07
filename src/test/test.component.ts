813286f52168445a8a8f77332f5e1b9084b5757e
SeraphCoding <yannikland@gmail.com>
2025-01-07 18:26:19 +0100

    Tailwindcss v4 does not load - example

--- src/test/test.component.ts
+++ src/test/test.component.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
   styleUrl: './test.component.scss'
 })
 export class TestComponent {
-
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
   plugins: [],
 }
